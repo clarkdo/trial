2789e77fc33ea41678e2519fa9b3f79e74e59761
Clark Du <clark.duxin@gmail.com>
2018-01-12 12:06:39 +0000

    First commit to GitLab

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import defaultInput from './input.json'
+
+window.visualize = (input) => console.log('TODO implement me')
+
+window.visualize(defaultInput)
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const { join } = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+  },
+  plugins: [
+    new HtmlPlugin({ template: 'src/index.html' }),
+  ],
+  devServer: {
+    port: 3000,
+  },
+}
